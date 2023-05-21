b2bfc7966a5e6570191cb99510ca1df0487e00be
unknown <leo_costab@outlook.com>
2023-05-21 09:08:46 -0300

    fix: change credit color

--- src/components/Credits/index.tsx
+++ src/components/Credits/index.tsx
@@ -4,7 +4,7 @@ const Credits =()=>{
     return (
         <>
             <Card bg='transparent' shadow={'none'}>
-                <Text bg={"transparent"} color={'#c1c1c1'}>Made by <Link textDecorationLine={'underline'} href="https://github.com/leltk">leltk</Link></Text>
+                <Text bg={"transparent"} color={'rgb(60 59 59);'}>Made by <Link textDecorationLine={'underline'} href="https://github.com/leltk">leltk</Link></Text>
             </Card>
         </>
     )
